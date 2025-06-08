ae16a7248e780d3a6b641735b1c684eb41578b07
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-06-08 20:30:07 +0300

    +

---
+++ Photo/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Photo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(26, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(64, 25);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "SIZE:";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(31, 47);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(92, 29);
+			this.radioButton1.TabIndex = 1;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "9 X 12";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(31, 82);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 29);
+			this.radioButton2.TabIndex = 2;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "12 X 15";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(31, 117);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 29);
+			this.radioButton3.TabIndex = 3;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "18 X 24";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(26, 159);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(92, 25);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "COUNT:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(155, 153);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(121, 31);
+			this.textBox1.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(31, 201);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(245, 39);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "OK";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(26, 267);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(79, 25);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Result:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+			this.ClientSize = new System.Drawing.Size(302, 334);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "PHOTO";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
