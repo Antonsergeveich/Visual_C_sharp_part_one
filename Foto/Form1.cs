3541d0d8f9fbb2f725308c4e1f91e76c813cec1d
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-06-08 17:56:50 +0300

    +

--- Foto/Form1.cs
+++ Foto/Form1.cs
@@ -15,6 +15,33 @@ namespace Foto
 		public Form1()
 		{
 			InitializeComponent();
+			//Настройка компонентов
+			radioButton1.Checked = true;
+			button1.Enabled = false;
+		}
+		//Щелчок по кнопке OK
+		private void button1_Click(object sender, EventArgs e)
+		{
+			double price = 0; //цена
+			int n;            //количество фотографий
+			double sum;       //сумма
+			if (radioButton1.Checked)
+			{
+				price = 8.50;
+			}
+			if(radioButton2.Checked)
+			{
+				price = 10;
+			}
+			if(radioButton3.Checked)
+			{
+				price = 15.5;
+			}
+			n = Convert.ToInt32(textBox1.Text);
+			sum = n * price;
+			label2.Text = "Price: " + price.ToString("C") + 
+				"\nCount: " + n.ToString() + "pieces\n" +
+				"Order sum: " + sum.ToString("C");
 		}
 	}
 }
