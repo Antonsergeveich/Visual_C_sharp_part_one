0c51b1e2f5fbfeb9c346121c0f9d1774d14e3f22
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-06-14 01:57:52 +0300

    +

--- Photo/Properties/1948/1948/main.cpp
+++ Photo/Properties/1948/1948/main.cpp
@@ -20,18 +20,26 @@ bool is_prime(int x)
 {
 	if (x > 1)
 	{
-		int i, j;
-		i = j = x;
-		while (i > 0)
+		int i, j, z;
+		i = j = z = x;
+		int y = 1;
+		while (i > 0 && i != 1)
 		{
-			i -= 2;
+			i = i - 2;
 		}
+		
 		while (j > 9)
 		{
 			j = j - 2;
 		}
-		
-		if (i == 1 || (j != 9 && j > 9))
+		if (i == 1 && j < 9) return true;
+		//if (i == 1 && j != 9 && j > 9 && j == 1)
+		while (z > 9)
+		{
+			z = z - 2;
+		}
+		if (i == 1 && z == 9 && j == 9)return true;
+		if (i == 1 && z != 9 && z > 9 && y == 1)
 		{
 			return true;
 		}
