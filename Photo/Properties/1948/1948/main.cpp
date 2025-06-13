1b1c2017cd448856083130da93752bb12f5468a2
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-06-14 00:33:50 +0300

    +

---
+++ Photo/Properties/1948/1948/main.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdbool.h>
+
+bool is_prime(int num);
+
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	if (is_prime(num) == true) {
+		printf("Yes");
+	}
+	else {
+		printf("No");
+	}
+
+}
+bool is_prime(int x)
+{
+	if (x > 1)
+	{
+		int i, j;
+		i = j = x;
+		while (i > 0)
+		{
+			i -= 2;
+		}
+		while (j > 9)
+		{
+			j = j - 2;
+		}
+		if (i == 1 || j != 9)
+		{
+			return true;
+		}
+		return false;
+	}
+	else
+	{
+		return false;
+	}
+}
